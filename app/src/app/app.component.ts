7696a16b31f73a069002e0508b463e0ed46f1097
Marc-Philippe Huget <mphuget@gmail.com>
2021-03-03 15:03:33 +0100

    Add the sidemenu

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Plugins } from '@capacitor/core';
+const { SplashScreen } = Plugins;
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  styleUrls: ['app.component.scss']
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit{
+  public selectedIndex = 0;
+  public appPages = [
+    {
+      title: 'Home',
+      url: '/tabs',
+      icon: 'home'
+    },
+    {
+      title: 'Pricing',
+      url: '/pricing',
+      icon: 'mail'
+    }
+  ];
+  constructor() {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    SplashScreen.hide();
+  }
+
+  ngOnInit() {
+    const path = window.location.pathname.split('/')[1];
+    console.log(path)
+    if (path !== undefined) {
+      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+    }
+  }
 }
